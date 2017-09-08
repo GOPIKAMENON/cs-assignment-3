fe84156d52365f85d78aa2707fef3823078d743b
GOPIKAMENON <31345164+GOPIKAMENON@users.noreply.github.com>
2017-09-08 18:55:49 +0530

    Add files via upload

---
+++ 3.24.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	
+	 char ch = 'a';
+
+	 cout<<("Alphabets from a - z are: ");
+    while(ch<='z')
+    {
+        cout<<(ch);
+        ch++;
+    }
+
+
+	return 0;
+}
